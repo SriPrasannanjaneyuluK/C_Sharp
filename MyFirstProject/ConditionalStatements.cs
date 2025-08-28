2e312fac70c010773e434eea52c965702ebd3f94
Anand Kalvakolanu <anandkalvakolanu@gmail.com>
2025-08-28 15:17:10 +0530

    Added ConditionalStatements.cs and updated project files

---
+++ MyFirstProject/ConditionalStatements.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class ConditionalStatements
+    {
+        public void ConditionChecking() 
+        {
+            int a = 10, b = 20;
+            if (a > b)
+            {
+                Console.WriteLine("A is greater than the B");
+            }
+            else 
+            {
+                Console.WriteLine("B is greater than the A");
+            }
+
+            Console.Write("Enter your first value1: ");
+            int value1  = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter your first value2: ");
+            int value2 = Convert.ToInt32(Console.ReadLine());
+
+            if (value1 > value2)
+            {
+                Console.WriteLine("Value 1 is greater than the Value 2");
+            }
+            else if (value1 < value2)
+            {
+                Console.WriteLine("value 1 smaller than the value 2");
+            }
+            else
+            { 
+                Console.WriteLine("Both values are equal");
+            }
+        }
+
+    }
+}
--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyFirstProject;
 
 namespace MyFirstProject
 {
@@ -13,7 +14,20 @@ namespace MyFirstProject
             Console.WriteLine("Hello anand"); 
             Console.WriteLine();
             Integers integers = new Integers();
+
+            // Call the integerDemo method to demonstrate integer operations
+            Console.WriteLine("Demonstrating Integer Operations:");
             integers.integerDemo();
+
+            // Create an instance of ConditionalStatements to demonstrate conditional statements
+            Console.WriteLine("\nDemonstrating Conditional Statements:");
+            ConditionalStatements conditionalStatements = new ConditionalStatements();
+
+            // The constructor of ConditionalStatements contains the demonstration code
+
+            conditionalStatements.ConditionChecking();
+
+            Console.ReadLine();
         }
     }
 }
